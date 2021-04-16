732492c99114c43ffd4a2deb5dea25859e00df78
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-04-16 16:24:14 +0200

    update version

--- src/main/java/com/supermartijn642/trashcans/TrashCans.java
+++ src/main/java/com/supermartijn642/trashcans/TrashCans.java
@@ -26,7 +26,7 @@ public class TrashCans {
 
     public static final String MODID = "trashcans";
     public static final String NAME = "Trash Cans";
-    public static final String VERSION = "1.0.8";
+    public static final String VERSION = "1.0.9";
 
     @Mod.Instance
     public static TrashCans instance;
