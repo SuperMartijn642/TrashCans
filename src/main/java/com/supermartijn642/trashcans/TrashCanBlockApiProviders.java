805d7ca9cef2872e60ae5cf5f3ca4b987a827691
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-11 15:44:18 +0200

    Overhaul and refactor mod

--- src/main/java/com/supermartijn642/trashcans/TrashCanBlockApiProviders.java
+++ src/main/java/com/supermartijn642/trashcans/TrashCanBlockApiProviders.java
@@ -14,15 +14,15 @@ public class TrashCanBlockApiProviders {
 
     public static void register(){
         ModLoadingContext.get().getActiveContainer().getEventBus().addListener((Consumer<RegisterCapabilitiesEvent>)event -> {
-            event.registerBlockEntity(Capabilities.Item.BLOCK, TrashCans.item_trash_can_tile, (entity, side) -> entity.ITEM_HANDLER);
-            event.registerBlockEntity(Capabilities.Item.BLOCK, TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.ITEM_HANDLER);
-            event.registerBlockEntity(Capabilities.Fluid.BLOCK, TrashCans.liquid_trash_can_tile, (entity, side) -> entity.FLUID_HANDLER);
-            event.registerBlockEntity(Capabilities.Fluid.BLOCK, TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.FLUID_HANDLER);
-            event.registerBlockEntity(Capabilities.Energy.BLOCK, TrashCans.energy_trash_can_tile, (entity, side) -> entity.ENERGY_STORAGE);
-            event.registerBlockEntity(Capabilities.Energy.BLOCK, TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.ENERGY_STORAGE);
+            event.registerBlockEntity(Capabilities.Item.BLOCK, TrashCans.item_trash_can_tile, (entity, side) -> entity.itemHandler);
+            event.registerBlockEntity(Capabilities.Item.BLOCK, TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.itemHandler);
+            event.registerBlockEntity(Capabilities.Fluid.BLOCK, TrashCans.liquid_trash_can_tile, (entity, side) -> entity.fluidHandler);
+            event.registerBlockEntity(Capabilities.Fluid.BLOCK, TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.fluidHandler);
+            event.registerBlockEntity(Capabilities.Energy.BLOCK, TrashCans.energy_trash_can_tile, (entity, side) -> entity.energyHandler);
+            event.registerBlockEntity(Capabilities.Energy.BLOCK, TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.energyHandler);
             if(Compatibility.MEKANISM.isInstalled()){
-                event.registerBlockEntity(Compatibility.MEKANISM.getGasHandlerCapability(), TrashCans.liquid_trash_can_tile, (entity, side) -> Compatibility.MEKANISM.getGasHandler(entity.liquidFilter, () -> entity.liquidFilterWhitelist));
-                event.registerBlockEntity(Compatibility.MEKANISM.getGasHandlerCapability(), TrashCans.ultimate_trash_can_tile, (entity, side) -> Compatibility.MEKANISM.getGasHandler(entity.liquidFilter, () -> entity.liquidFilterWhitelist));
+                event.registerBlockEntity(Compatibility.MEKANISM.getGasHandlerCapability(), TrashCans.liquid_trash_can_tile, (entity, side) -> entity.gasHandler);
+                event.registerBlockEntity(Compatibility.MEKANISM.getGasHandlerCapability(), TrashCans.ultimate_trash_can_tile, (entity, side) -> entity.gasHandler);
             }
         });
     }
