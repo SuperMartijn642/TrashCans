d84dcd5511fa47b8849a64c53feaf8a2ca2882e0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-11 15:44:18 +0200

    Overhaul and refactor mod

--- src/main/java/com/supermartijn642/trashcans/screen/ItemTrashCanScreen.java
+++ src/main/java/com/supermartijn642/trashcans/screen/ItemTrashCanScreen.java
@@ -6,11 +6,15 @@ import com.supermartijn642.core.gui.ScreenUtils;
 import com.supermartijn642.trashcans.TrashCanBlockEntity;
 import com.supermartijn642.trashcans.TrashCans;
 import com.supermartijn642.trashcans.packet.PacketToggleItemWhitelist;
+import com.supermartijn642.trashcans.screen.components.WhitelistButton;
+import net.minecraft.resources.ResourceLocation;
 
 /**
  * Created 7/11/2020 by SuperMartijn642
  */
 public class ItemTrashCanScreen extends TrashCanScreen<ItemTrashCanContainer> {
+
+    private static final ResourceLocation BACKGROUND = TrashCans.identifier("textures/item_screen.png");
 
     private WhitelistButton whitelistButton;
 
@@ -21,18 +25,18 @@ public class ItemTrashCanScreen extends TrashCanScreen<ItemTrashCanContainer> {
     @Override
     protected void addWidgets(TrashCanBlockEntity entity){
         this.whitelistButton = this.addWidget(new WhitelistButton(175, this.height() - 118, () -> TrashCans.CHANNEL.sendToServer(new PacketToggleItemWhitelist(this.container.getBlockEntityPos()))));
-        this.whitelistButton.update(entity.itemFilterWhitelist);
+        this.whitelistButton.update(entity.isItemFilterWhitelist());
     }
 
     @Override
     protected void update(TrashCanBlockEntity entity){
         super.update(entity);
-        this.whitelistButton.update(entity.itemFilterWhitelist);
+        this.whitelistButton.update(entity.isItemFilterWhitelist());
     }
 
     @Override
-    protected String getBackground(){
-        return "item_screen.png";
+    protected ResourceLocation getBackground(){
+        return BACKGROUND;
     }
 
     @Override
