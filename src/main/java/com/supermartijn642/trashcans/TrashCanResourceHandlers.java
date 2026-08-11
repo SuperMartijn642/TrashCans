3b8fd2f2ee5842cfa6071a430d9ddd32d86f8048
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-12 00:55:59 +0200

    Store and allow recovering the last 6 deleted items

--- src/main/java/com/supermartijn642/trashcans/TrashCanResourceHandlers.java
+++ src/main/java/com/supermartijn642/trashcans/TrashCanResourceHandlers.java
@@ -79,7 +79,11 @@ public class TrashCanResourceHandlers {
 
         @Override
         public @NotNull ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate){
-            return this.isItemValid(0, stack) ? ItemStack.EMPTY : stack;
+            if(!this.isItemValid(0, stack))
+                return stack;
+            if(!simulate)
+                this.entity.pushDeletedItem(stack.copy());
+            return ItemStack.EMPTY;
         }
 
         @Override
